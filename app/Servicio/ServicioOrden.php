2cf45ad94db6fc3b493fb04adf00ddd8f85b0035
Elias <martinezgarcialuiselias8@gmail.com>
2026-02-16 22:47:42 -0600

    cambio minimo 1

--- app/Servicio/ServicioOrden.php
+++ app/Servicio/ServicioOrden.php
@@ -26,7 +26,7 @@ $status (opcional)
         if (isset($objeto->fecha))
             $o->canal=$objeto->canal;
         else
-            $o->canal='WEB';
+            $o->canal='APP';
 
         $o->idcanal=$objeto->idcanal;
 
